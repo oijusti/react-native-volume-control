d8959a55c3de6a440dff51f771efb63de43b7d77
puckey <jonathan@puckey.studio>
2022-01-12 10:44:04 +0100

    Add missing methods

--- android/src/main/java/com/rtmalone/volumecontrol/RNVolumeControlModule.java
+++ android/src/main/java/com/rtmalone/volumecontrol/RNVolumeControlModule.java
@@ -41,12 +41,7 @@ public class RNVolumeControlModule extends ReactContextBaseJavaModule
   }
 
   @Override
-  public void onNewIntent(Intent intent) {
-  }
-
-  @Override
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-  }
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {}
 
   @Override
   public void onHostResume() {
@@ -59,8 +54,13 @@ public class RNVolumeControlModule extends ReactContextBaseJavaModule
   }
 
   @Override
-  public void onHostDestroy() {
-  }
+  public void onHostDestroy() {}
+
+  @ReactMethod
+  public void addListener(String eventName) {}
+
+  @ReactMethod
+  public void removeListeners(Integer count) {}
 
   @Override
   public void initialize() {
